b9bd66fd69f23b459edf1e62543c276f72a5fe87
DumDumSu <subrain2@gmail.com>
2025-12-03 01:53:17 +0000

    fixing fixing fixing

--- inc/Flightplanner.cpp
+++ inc/Flightplanner.cpp
@@ -47,13 +47,13 @@ static Vertex *findVertex(Graph &g, const std::string &code) {
             return g.vertices[i];
     }
     return nullptr;
-};
+}
 
 // we weight all our codes by in the following manner
 // 1 = cheapest, 2 = fastest, 3 = fewest stops
 
 static void buildWeightedGraph(Graph &g, int pref) {
-    for (int i = 0; i < g.vertices.size() i++) {
+    for (int i = 0; i < g.vertices.size(); i++) {
         g.vertices[i]->edgeList = ArrayList<Edge *>();
     }
 
@@ -74,23 +74,59 @@ static void buildWeightedGraph(Graph &g, int pref) {
     }
 }
 
-FlightResults computePath(Graph *g, const std::string &start,
-                          const std::string &dest, int preference) {
+static std::vector<std::string> buildPath(Waypoint *end) {
+    std::vector<std::string> path;
 
+    while (end != nullptr) {
+        path.push_back(end->vertex->data);
+        end = end->parent;
+    }
+
+    std::reverse(path.begin(), path.end());
+    return path;
+}
+
+FlightResults computePath(Graph &g, const std::string &start,
+                          const std::string &dest, int preference) {
     FlightResults out;
     out.totalPrice = 0;
     out.stops = 0;
     out.totalTime = 0;
 
-    buildWeightedGraph(int g, preference);
+    buildWeightedGraph(g, preference);
 
-    Vertex *sV = findVertex( *g, start);
-    Vertex *dV = findVertex( *g, dest);
+    Vertex *sV = findVertex(g, start);
+    Vertex *dV = findVertex(g, dest);
 
     if (!sV || !dV)
         return out;
 
     Waypoint *resultNode = nullptr;
 
-    
+    if (preference == 3) {
+        resultNode = g.bfs(sV, dV);
+
+    } else {
+        resultNode = g.ucs(sV, dV);
+    }
+
+    if (!resultNode)
+        return out;
+    // pathing
+    out.path = buildPath(resultNode);
+    out.stops = out.path.size() - 1;
+
+    for (int i = 0; i < out.path.size() - 1; i++) {
+        std::string a = out.path[i];
+        std::string b = out.path[i + 1];
+
+        for (auto &r : routes) {
+            if (r.src == a && r.dest == b) {
+                out.totalPrice += r.price;
+                out.totalTime += r.time;
+            }
+        }
+    }
+
+    return out;
 }
